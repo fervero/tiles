eff807c804c8d025da399f6e0b1e74883d3cd630
Maciej Bojko <maciej.bojko@gmail.com>
2018-02-04 16:01:21 +0100

    Implemented pinned tiles

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import './Tile.css';
 import TileItem from '../TileItem/TileItem';
+import { CSSTransitionGroup } from 'react-transition-group';
+import {PinIcon} from 'react-octicons'
 
 const filterItems = (search) => (
 	({ description }) => description.toLowerCase().includes(search)
 )
 
-const makeRow = ({...props}) => (
-	<TileItem {...props} />
+const makeRow = ({ id, externalURL, description }) => (
+	<TileItem key={id} externalURL={externalURL} description={description} />
 );
 
 function Tile(props) {
+	function handleClick(e) {
+		props.toggle(props.name);
+	}
+	console.log(props);
+	const btnStyling = props.pinned ? " btn-dark" : "";
 	return (
-		<section className="tile card">
-			<h2 className="list-group-header">{props.name}</h2>
+		<section className="tile">
+			<div className="list-group-header">
+				<h2 className="tile__header">{props.name}</h2>
+				<button className={"btn tile__btn" + btnStyling} onClick={handleClick}><PinIcon /></button>
+			</div>
 			<ul className="list-group list-group-flush">
-				{props.entries
-					.filter(filterItems(props.search))
-					.map(makeRow)}
+				<CSSTransitionGroup
+					transitionName="example"
+					transitionEnterTimeout={1500}
+					transitionLeaveTimeout={1500}
+				>
+					{props.entries
+						.filter(filterItems(props.search))
+						.map(makeRow)}
+				</CSSTransitionGroup>
 			</ul>
 		</section>
 	)
--- src/Tiles/Tiles.js
+++ src/Tiles/Tiles.js
@@ -3,16 +3,6 @@ import Tile from '../Tile/Tile';
 import Navbar from '../Navbar/Navbar';
 import './Tiles.css';
 
-const makeTile = ({ name, entries, search }) => (
-	<div className="tiles_container" key={name}>
-		<Tile name={name} entries={entries} search={search} />
-	</div>
-)
-
-const filterTiles = (search) => (
-	({ name }) => name.toLowerCase().includes(search)
-)
-
 const applySearch = (search) => (
 	(obj) => Object.assign({}, obj, { search })
 )
@@ -21,25 +11,59 @@ class Tiles extends Component {
 	constructor(props) {
 		super(props);
 		this.model = JSON.parse(JSON.stringify(props.model));
+		const tilesList = this.model
+			.map(() => false);
 		this.brand = props.brand;
-		this.state = { search: "" };
+		this.state = {
+			search: "",
+			tileIsPinned: tilesList
+		};
 		this.updateSearch = this.updateSearch.bind(this);
+		this.togglePinned = this.togglePinned.bind(this);
+		this.makeTile = this.makeTile.bind(this);
 	}
+
+	makeTile = ({ name, entries, search }, index) => (
+		<div className="tiles_container" key={name}>
+			<Tile name={name} entries={entries} search={search} toggle={this.togglePinned} pinned={this.state.tileIsPinned[index]}/>
+		</div>
+	)
 
 	updateSearch(e) {
 		const search = e.target.value.toLowerCase();
 		this.setState({ search });
 	}
 
+	togglePinned(tileName) {
+		const foundTile = this.model
+			.findIndex(({ name }) => name === tileName);
+		const newStatus = this.state.tileIsPinned
+			.map((val, i) => i === foundTile ? !val : val);
+		this.setState(Object.assign(
+			{}, this.state, { tileIsPinned: newStatus }
+		));
+		setTimeout(() => console.log(this.state), 50);
+	}
+
+	/**
+	 * Tiles stays on display if it's pinned or if its name contains the search term.
+	 */
+	filterTiles = (search) => (
+		({ name }, index) => (
+			name.toLowerCase().includes(search.toLowerCase())
+		) || (this.state.tileIsPinned[index])
+	)
+
 	render() {
+		const search = this.state.search;
 		return (
 			<div className="container">
 				<Navbar brand={this.brand} updateSearch={this.updateSearch} />
 				<div className="row">
 					{this.model
-						.filter(filterTiles(this.state.search))
-						.map(applySearch(this.state.search))
-						.map(makeTile)}
+						.filter(this.filterTiles(search))
+						.map(applySearch(search))
+						.map(this.makeTile)}
 				</div>
 			</div>
 		)
